48e7b4b066af367731055bb1486d37d2513401e3
yuuta <mks.btc@mail.ru>
2023-09-21 23:11:22 -0600

    vistas de recursos intento de menu flotante deplegable aun sin funcionar

---
+++ js/menuFlotante.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOMContentLoaded event fired.");
+    const nav = document.querySelector("#nav");
+    const abrir = document.querySelector("#abrir");
+    const cerrar = document.querySelector("#cerrar");
+    const abrirLateral = document.querySelector("#abrir-lateral");
+
+    abrir.addEventListener("click", () => {
+        nav.classList.add("visible");
+    });
+
+    cerrar.addEventListener("click", () => {
+        nav.classList.remove("visible");
+    });
+
+    abrirLateral.addEventListener("click", () => {
+        nav.classList.add("visible");
+    });
+});
