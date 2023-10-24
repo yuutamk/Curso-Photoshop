fb225ea33035ee31ed7c5695bb1c021540c78ec2
yuuta <mks.btc@mail.ru>
2023-10-23 23:30:56 -0600

    intento de flecha

--- js/menuFlotante.js
+++ js/menuFlotante.js
@@ -1,19 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("DOMContentLoaded event fired.");
- /*    const nav = document.querySelector("#nav"); */
-    const abrir = document.querySelector("#abrir");
-    const cerrar = document.querySelector("#cerrar");
-    const abrirLateral = document.querySelector("#abrir-lateral");
-
-    abrir.addEventListener("click", () => {
-        nav.classList.add("visible");
-    });
-
-    cerrar.addEventListener("click", () => {
-        nav.classList.remove("visible");
-    });
-
-    abrirLateral.addEventListener("click", () => {
-        nav.classList.add("visible");
+$(document).ready(function () {
+    $('#toggle-button').click(function () {
+        $('.menu-lateral.mobile').toggleClass('open');
+        $('.contenido').toggleClass('open');
+        $('body').toggleClass('main-menu-visible');
     });
 });
