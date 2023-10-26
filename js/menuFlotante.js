074f928f7fe953a09a13d6d1d61a051ff1134e04
yuuta <mks.btc@mail.ru>
2023-10-26 02:09:22 -0600

    intento de flecha

--- js/menuFlotante.js
+++ js/menuFlotante.js
@@ -1,7 +1,18 @@
-$(document).ready(function () {
-    $('#toggle-button').click(function () {
-        $('.menu-lateral.mobile').toggleClass('open');
-        $('.contenido').toggleClass('open');
-        $('body').toggleClass('main-menu-visible');
-    });
+const abrirMenu = document.getElementById("menu-toggle");
+const toggleButton = document.getElementById("toggle-button");
+
+// Agrega un evento clic al botón de toggle
+abrirMenu.addEventListener("click", () => {
+    const menuLateral = document.querySelector(".menu-lateral.mobile"); // Selecciona el menú lateral
+
+    // Alternar la clase "visible" en el menú
+    menuLateral.classList.toggle("visible");
+
+    // Si el menú está visible, ajusta el margen del contenido principal
+    const contenidoPrincipal = document.querySelector(".contenido");
+    if (menuLateral.classList.contains("visible")) {
+        contenidoPrincipal.style.marginLeft = "0"; // o cualquier otro valor necesario
+    } else {
+        contenidoPrincipal.style.marginLeft = "20%"; // o el margen deseado
+    }
 });
